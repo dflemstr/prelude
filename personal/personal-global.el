478069b7449c3aac051bbd0f0c10d2f84d2650ca
David Flemström <david.flemstrom@gmail.com>
2013-09-02 19:14:38 +0200

    Add a global variables personal file

---
+++ personal/personal-global.el
@@ -0,0 +1,10 @@
+;;; personal-global.el --- Personal: Global customizations
+
+;;; Commentary:
+
+;;; Code:
+
+(setq system-time-locale "en_US.utf8")
+
+(provide 'personal-global)
+;;; personal-global.el ends here
