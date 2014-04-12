cda0100f66a3399412877884d7230b5dc8ce62c0
David Flemström <david.flemstrom@gmail.com>
2014-04-12 20:13:14 +0200

    Add personal-c.el

---
+++ personal/personal-c.el
@@ -0,0 +1,12 @@
+;;; personal-c.el --- Personal: Some C language defaults
+
+;;; Commentary:
+
+;; Sets up defaults for handling C-like code
+
+;;; Code:
+
+(setq indent-tabs-mode nil)
+
+(provide 'personal-c)
+;;; personal-c.el ends here
